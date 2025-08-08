003926d378e196f4dfb23e408d506efe28e0cd5c
R4sl4n <abueyub1@gmail.com>
2025-08-08 16:33:16 +0300

    add test renderBoard

--- spec/sudoku.spec.js
+++ spec/sudoku.spec.js
@@ -1,1 +1,14 @@
-const sudoku = require('../sudoku.js')
+const renderBoard = require('../sudoku.js')
+const sudoku = require('../sudoku.js');
+
+
+describe('renderBoard', () => { 
+    test('должен выбросить ошибку при несуществующем индексе', () => {
+    const boards = ['123456789123456789123456789123456789123456789123456789123456789123456789123456789'];
+    
+    expect(() => {
+      renderBoard(boards, 5);
+    }).toThrow();
+  });
+});
+
